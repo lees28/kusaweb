8342741905df11d30698c1df306030670d0f856d
Alex Song <ss456@duke.edu>
2014-08-17 02:37:54 -0400

    Revert "fixed sendgrid email issue"
    This reverts commit d3c07ac8232fb9f7baa85a87eac7444783a0591d.

--- KUSA/settings.py
+++ KUSA/settings.py
@@ -111,9 +111,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 import os
-EMAIL_HOST_USER = 'app25920742@heroku.com'
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
 EMAIL_HOST= 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = 'nm2vvbuu'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
