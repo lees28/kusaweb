a617b60c945229f0cfffa9a7076ae7030ba74b88
alexsong93 <ss456@duke.edu>
2015-03-04 19:17:41 -0500

    also adding views.py file

--- KUSAapp/views.py
+++ KUSAapp/views.py
@@ -11,7 +11,14 @@ import os
 def homepage(request):
     with open("templates/exec.json") as json_file:
         execs = json.load(json_file)
-        return render(request, 'english.html', {"execs":execs})
+
+        photo_urls = {}
+        lines = []
+        for filename in os.listdir("KUSAapp/static/photo_urls"):
+            lines = [line.strip() for line in open("KUSAapp/static/photo_urls/" + filename)]
+            photo_urls[filename] = lines
+        photo_urls_len = len(photo_urls)
+        return render(request, 'english.html', {"execs":execs, "photo_urls":photo_urls, "photo_urls_len":photo_urls_len})
 
 def contact(request):
     send_mail(
