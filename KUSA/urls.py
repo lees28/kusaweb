d482ab0daf2c02f3832d9d8a029c6a0075664c32
Soo Lee <soo.yeon.lee@vanderbilt.edu>
2016-07-21 09:48:05 +0900

    init

--- KUSA/urls.py
+++ KUSA/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -11,12 +12,23 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     
     #homepage
-    url(r'^/?$','KUSAapp.views.homepage'),
+    url(r'^$', 'KUSAapp.views.home'),
+
+    url(r'^about/?$','KUSAapp.views.about',name='about'), 
+    url(r'^board/$','KUSAapp.views.board',name='board'),                                                                     
+    url(r'^photos/?$','KUSAapp.views.photos',name='photos'),    
+    url(r'^events/?$','KUSAapp.views.events',name='events'),                                                                     
+    url(r'^contact/?$','KUSAapp.views.contact',name='contact'),                                                                     
+                                                            
+                                                                    
+
+        #url(r'^/?$','KUSAapp.views.homepage'),
+
     
     #different languages
     # url(r'^eng/?$', 'KUSAapp.views.engHomepage'),
     # url(r'^kor/?$', 'KUSAapp.views.korHomepage'),
     
     # contact form
-    url(r'^contact/?$', 'KUSAapp.views.contact', name='contact'),
+    url(r'^contact/?$', 'KUSAapp.views.contact', name='contact')
 )
--- KUSAapp/views.py
+++ KUSAapp/views.py
@@ -2,26 +2,49 @@ from django.shortcuts import render
 from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
 from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.template import RequestContext
+
+
 import json
 import os
 
 # Create your views here.
 
-def homepage(request):
-    with open("templates/exec.json") as json_file:
-        execs = json.load(json_file)
+def home(request):
+    #html="Welcome page" <a href="/KUSA/base.html">About</a>
 
-        photo_urls = []
-        lines = []
-        filenames = ["Springternational", "KoreaNight2015", "KoreanLunarNewYear2015", "SaveMySeoul", 
-                     "BBQ2014", "BowlingNight", "Chuseok2014", "Kickoff2014", "CareerNight", 
-                     "YearOfTheHorse", "KoreaNight-SNL", "Chuseok2013"]
-        base_dir = "KUSAapp/static/photo_urls/"
-        for filename in filenames:
-            lines = [line.strip() for line in open(base_dir + filename)]
-            photo_urls.append(lines)
-        return render(request, 'english.html', {"execs":execs, "photo_urls":photo_urls, "filenames":filenames})
+    return render(request,'index1.html',{})
+
+
+def about(request):
+    return render(request, 'base.html',{})
+
+def board(request):
+    return render(request, 'exec.html',{})
+
+def photos(request):
+    return render(request, 'photos.html',{})
+
+
+
+#def homepage(request):
+
+#    with open("templates/exec.json") as json_file:
+#        execs = json.load(json_file)
+
+#        photo_urls = []
+#        lines = []
+#        filenames = ["Springternational", "KoreaNight2015", "KoreanLunarNewYear2015", "SaveMySeoul", 
+#                     "BBQ2014", "BowlingNight", "Chuseok2014", "Kickoff2014", "CareerNight", 
+#                     "YearOfTheHorse", "KoreaNight-SNL", "Chuseok2013"]
+#        base_dir = "KUSAapp/static/photo_urls/"
+#        for filename in filenames:
+#            lines = [line.strip() for line in open(base_dir + filename)]
+#            photo_urls.append(lines)
+#        return render(request, 'english.html', {"execs":execs, "photo_urls":photo_urls, "filenames":filenames})
 
 def contact(request):
     send_mail(
@@ -33,5 +56,4 @@ def contact(request):
     )
     return HttpResponseRedirect('/')
 
-    
 
